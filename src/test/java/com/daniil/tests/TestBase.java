3dd7ca81b53c5b051586908fdd06c606304ea259
danii <daniil.kravchenko.92@yandex.ru>
2022-01-25 22:24:34 +0300

    copied tests for jenkins

--- src/test/java/com/daniil/tests/TestBase.java
+++ src/test/java/com/daniil/tests/TestBase.java
@@ -17,9 +17,9 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        // Подключаем Selenoid
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 //        Configuration.remote = System.getProperty("remote_driver_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
